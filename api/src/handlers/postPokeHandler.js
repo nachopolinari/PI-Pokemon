c33aa9aa511af16f30cc5d15128fffc4035f699b
Nacho Polinari <nachopolinari@gmail.com>
2023-08-31 01:02:29 -0300

    pequeños cambios en el handler postPokeHandler

--- api/src/handlers/postPokeHandler.js
+++ api/src/handlers/postPokeHandler.js
@@ -1,10 +1,10 @@
-const {createNewPoke} =require('../controllers/createNewPoke')
+const { createNewPoke } = require('../controllers/createNewPoke')
 const postPokeHandler = async (req, res) => {
   // Esta ruta recibirá todos los datos necesarios para crear un pokemon y relacionarlo con sus tipos solicitados.
   // Toda la información debe ser recibida por body.
   // Debe crear un pokemon en la base de datos, y este debe estar relacionado con sus tipos indicados (debe poder relacionarse al menos con dos).
+  const { name, img, life, attack, defense, speed, height, weight } = req.body
   try {
-    const { name, img, life, attack, defense, speed, height, weight } = req.body
     const newPoke = await createNewPoke(name, img, life, attack, defense, speed, height, weight);
     res.status(201).json(newPoke);
 
@@ -14,7 +14,7 @@ const postPokeHandler = async (req, res) => {
   }
 
   // res.status(200).send(`NIY: Esta ruta crea/postea un pokemon con estos datos:
-  
+
   //   name:${name},
   //   img:${img},
   //   life:${life},
--- api/src/models/Pokemon.js
+++ api/src/models/Pokemon.js
@@ -4,43 +4,44 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('pokemon', {
-    id:{
-      type:DataTypes.UUID,
-      defaultValue:DataTypes.UUIDV1,
-      primaryKey:true,
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV1,
+      primaryKey: true,
     },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
-      unique:true
+      unique: true
     },
-    img:{
-      type:DataTypes.TEXT,
-      allowNull:false,
+    img: {
+      type: DataTypes.TEXT,
+      allowNull: false,
     },
-    life:{
-      type:DataTypes.FLOAT,
-      allowNull:false,
+    life: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
     },
-    attack:{
-      type:DataTypes.FLOAT,
-      allowNull:false,
+    attack: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
     },
-    defense:{
-      type:DataTypes.FLOAT,
-      allowNull:false,
+    defense: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
     },
-    speed:{
-      type:DataTypes.FLOAT,
-      allowNull:false,
+    speed: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
     },
-    height:{
-      type:DataTypes.FLOAT,
+    height: {
+      type: DataTypes.FLOAT,
     },
-    weight:{
-      type:DataTypes.FLOAT,
-    }
-  },{
-    timestamps:false
+    weight: {
+      type: DataTypes.FLOAT,
+    },
+    
+  }, {
+    timestamps: false
   });
 };
