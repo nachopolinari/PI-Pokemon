cfcf9bb60ad949333abc1f5177ab9f78e2ee9873
Nacho Polinari <nachopolinari@gmail.com>
2023-09-03 15:23:39 -0300

    rutas views creadas

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,17 @@
+import { Landing, Form, Home, Detail } from "./Views";
+import { Routes, Route } from 'react-router-dom';
+
 
 function App() {
   return (
     <div className="App">
       <h1>Todo ok</h1>
+      <Routes>
+        <Route path='/' element={<Landing />} />
+        <Route path='/home' element={<Home />} />
+        <Route path='/detail' element={<Detail />} />
+        <Route path='/form' element={<Form />} />
+      </Routes>
     </div>
   );
 }
